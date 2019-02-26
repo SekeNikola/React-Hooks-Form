0a45cbc3ae2433f6d8050e6ec06844a84e60d284
Seke Nikola <sekenikola@gmail.com>
2019-02-26 14:11:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import React, { useState } from "react";
+import "./App.css";
+const App = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let [showName, setShowName] = useState(false);
+  return (
+    <>
+      <form>
+        <input
+          type="text"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+          placeholder="First name"
+          name="firstName"
+          required
+        />
+        <input
+          type="text"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+          placeholder="Last name"
+          name="lastName"
+          required
+        />
+        <input
+          type="text"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          placeholder="Email"
+          name="Email"
+          required
+        />
+        <input
+          type="text"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          placeholder="Password"
+          name="password"
+          required
+        />
+        <button
+          type="submit"
+          onClick={e => {
+            e.preventDefault();
+            setShowName(true);
+          }}
+        >
+          Submit
+        </button>
+        {showName && (
+          <>
+            <p>FirstName: {firstName}</p>
+            <p>Last Name:{lastName}</p>
+          </>
+        )}
+      </form>
+    </>
+  );
+};
 
 export default App;
